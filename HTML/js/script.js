1d9c43571627fde1f793e5b18901e938780f3b86
Sol Jin <soljin01@gmail.com>
2017-04-04 13:22:41 -0400

    fix slideshow js

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -144,4 +144,24 @@ window.onclick = function(event) {
     $(".parallax-background-partners").parallax();
   }
 
+  //slideshow
+  var slideIndex = 1;
+  showDivs(slideIndex);
+
+  function plusDivs(n) {
+    event.preventDefault();
+    showDivs(slideIndex += n);
+  }
+
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlide");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+  }
+
 });
